7e7be28fe8d530012f2f6068dd66c1a6fea39941
shadiknaushad <ms1819050@gmail.com>
2025-06-28 09:38:03 +0530

    sixth commit

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -11,7 +11,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Catalog App"), backgroundColor: Colors.blue),
+      appBar: AppBar(title: Text("Catalog App"), backgroundColor: Colors.blueGrey),
       body: Center(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
